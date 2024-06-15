5c8d43486555090e8da77f9554a1d7dd1a04c210
Julia <veluat@gmail.com>
2024-06-15 12:34:13 +0300

    fix: uploader file bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,40 @@ import './App.css';
 import DesignImage from './logo.svg'
 
 function App() {
-    const [file, setFile] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [imgSrc, setImgSrc] = useState<string>(DesignImage);
 
-    const handleFile = (event: React.MouseEvent<HTMLLabelElement, MouseEvent>) => {
-        const selectedFile = (event.target as HTMLInputElement).files?.[0] || null;
-        if (selectedFile) {
-            const fileUrl = URL.createObjectURL(selectedFile);
-            setFile(fileUrl)
-        }
-    };
-
-    const HandleDelete = () => {
-        setFile(null)
+  const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setImgSrc(URL.createObjectURL(selectedFile));
     }
+  };
 
-    let imgSrc = file === null ? DesignImage : file;
-    let btnName = file === null ? 'Добавить' : 'Изменить'
-    return (
-        <div className='App'>
-            <div className='imgWrap'>
-                <img src={imgSrc} alt='img'/>
-            </div>
-            <div className='btnWrap'>
-                <label htmlFor="file-upload" className='labelBtn' onClick={handleFile}>
-                    <input type="file" id="file-upload"/>
-                    {btnName}
-                </label>
-                {btnName === 'Изменить' && (
-                    <div className='Btn' onClick={HandleDelete}>Удалить</div>
-                )}
-            </div>
+  const handleDelete = () => {
+    setFile(null);
+    setImgSrc(DesignImage);
+  }
 
-        </div>
-    );
+  let btnName = file === null ? 'Добавить' : 'Изменить';
+
+  return (
+    <div className='App'>
+      <div className='imgWrap'>
+        <img src={imgSrc} alt='img'/>
+      </div>
+      <div className='btnWrap'>
+        <label htmlFor="file-upload" className='labelBtn'>
+          <input type="file" id="file-upload" onChange={handleFile}/>
+          {btnName}
+        </label>
+        {btnName === 'Изменить' && (
+          <div className='Btn' onClick={handleDelete}>Удалить</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
